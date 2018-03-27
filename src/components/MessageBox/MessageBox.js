90eb2d79020b6ae9943d7d5826957391d58dc4ed
ysdkmn <ysdkmn@gmail.com>
2018-03-27 12:06:59 -0400

    added message box that warns when neccessary values are not input and confirms that playlist was saved to spotify

---
+++ src/components/MessageBox/MessageBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './MessageBox.css';
+
+class MessageBox extends React.Component {
+  render() {
+    return (
+      <div className={`MessageBox ${this.props.message.state}`}>
+        <div className="Message-container">
+          <h1>{this.props.message.message}</h1>
+          <a onClick={this.props.handleMessageOff}>
+            {this.props.message.button}
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MessageBox;
